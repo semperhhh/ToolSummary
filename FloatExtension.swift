186e41f0f927004d92cbb79fc05728b0271111d3
semperhhh <zphui5409@163.com>
2021-01-12 23:55:07 +0800

    Create FloatExtension.swift

---
+++ FloatExtension.swift
@@ -0,0 +1,24 @@
+extension Float {
+
+    // 四舍五入
+    var toInt: Int {
+        Int(self.rounded())
+    }
+    // 入
+    var toIntDown: Int {
+        Int(self.rounded(.down))
+    }
+    // 舍
+    var toIntUp: Int {
+        Int(self.rounded(.up))
+    }
+    
+    func toRounded(mult: Int) -> Float {
+        guard mult > 0 else {
+            return self.rounded()
+        }
+        let div: Decimal = pow(10, mult)
+        let m: Float = Float(String(div.description)) ?? 0
+        return (self * m).rounded() / m
+    }
+}
