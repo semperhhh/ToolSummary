79752483b195b1e7ead6925417f770b244817d29
semperhhh <zphui5409@163.com>
2021-01-12 23:29:27 +0800

    Create IntExtension.swift

---
+++ IntExtension.swift
@@ -0,0 +1,26 @@
+enum IntTimeUnit {
+    case seconds, minutes, hours
+}
+
+extension Int {
+    
+    /// int转时间格式化
+    /// - Parameter dateformatter: 时间的格式
+    /// - Returns: 时间字符串
+    func toTimeFormatter(_ units: IntTimeUnit = .seconds, dateformatter: String = "HH:mm:ss") -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateformatter
+        var c = DateComponents()
+        switch units {
+        case .seconds:
+            c.second = self
+        case .minutes:
+            c.minute = self
+        default:
+            c.hour = self
+        }
+        let d: Date = Calendar.current.date(from: c) ?? Date()
+        let str = formatter.string(from: d)
+        return str
+    }
+}
