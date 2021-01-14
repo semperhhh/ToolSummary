c65765c1337257118319c2c50af02da2836d5f77
semperhhh <zphui5409@163.com>
2021-01-14 19:49:09 +0800

    Create ArrayExtension.swift

---
+++ ArrayExtension.swift
@@ -0,0 +1,29 @@
+extension Dictionary {
+    /// 字典转json
+    var toJSONString: String {
+        guard JSONSerialization.isValidJSONObject(self) else {
+            print("无法解析出JSONString")
+            return ""
+        }
+        guard let data = try? JSONSerialization.data(withJSONObject: self, options: []) else {
+            return ""
+        }
+        let jsonString = String(data: data, encoding: .utf8)
+        return jsonString ?? ""
+    }
+}
+
+extension Array {
+    /// 字典转json
+    var toJSONString: String {
+        guard JSONSerialization.isValidJSONObject(self) else {
+            print("无法解析出JSONString")
+            return ""
+        }
+        guard let data = try? JSONSerialization.data(withJSONObject: self, options: []) else {
+            return ""
+        }
+        let jsonString = String(data: data, encoding: .utf8)
+        return jsonString ?? ""
+    }
+}
