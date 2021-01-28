5b9caa5c8839c725c3b53976aa324a74394c0efd
semperhhh <zphui5409@163.com>
2021-01-28 18:11:06 +0800

    Update DateExtension.swift

--- DateExtension.swift
+++ DateExtension.swift
@@ -1,28 +1,29 @@
 extension Date {
 
-    // 获取UTC-8时区的年
-    func currentYear() -> String {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "zh_CN")
-        formatter.timeZone = TimeZone(abbreviation: "UTC+8")
-        formatter.dateFormat = "yyyy"
-        return formatter.string(from: self)
-    }
-
-    // 获取UTC-8时区的月
-    func currentMonth() -> String {
-        let formatter = DateFormatter()
-        formatter.locale = Locale(identifier: "zh_CN")
-        formatter.timeZone = TimeZone(abbreviation: "UTC+8")
-        formatter.dateFormat = "MM"
-        return formatter.string(from: self)
-    }
     // 获取UTC-8时区的天
-    func currentDay() -> String {
+    var currentDay: String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "zh_CN")
         formatter.timeZone = TimeZone(abbreviation: "UTC+8")
         formatter.dateFormat = "dd"
-        return formatter.string(from: self)
+        return formatter.string(from: base)
+    }
+
+    // 获取UTC-8时区的月
+    var currentMonth: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "zh_CN")
+        formatter.timeZone = TimeZone(abbreviation: "UTC+8")
+        formatter.dateFormat = "MM"
+        return formatter.string(from: base)
+    }
+
+    // 获取UTC-8时区的年
+    var currentYear: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "zh_CN")
+        formatter.timeZone = TimeZone(abbreviation: "UTC+8")
+        formatter.dateFormat = "yyyy"
+        return formatter.string(from: base)
     }
 }
